05cb50db8cd39e6cf94a345c63f517bbd84bcac6
Tronerta <tronerta.web@gmail.com>
2023-02-16 15:59:08 +0100

    Trip deletion fix

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -7,6 +7,7 @@ class Trip < ApplicationRecord
   has_many :places, through: :trip_place_infos, dependent: :destroy
   has_many :journeys, dependent: :destroy
   has_many :user_favorite_trips, dependent: :destroy
+  has_many :ratings, dependent: :destroy
 
   def favorite
     UserFavoriteTrip.where(user_id: current_user.id, trip_id: id).present?
